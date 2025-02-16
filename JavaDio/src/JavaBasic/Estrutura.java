3abde5d84e73bbf4c29921b35ab5ef964f40002a
Rafael Queiroz <rafael_queiroz144@hotmail.com>
2025-02-16 01:46:37 -0300

    Subindo Projeto Teste

---
+++ JavaDio/src/JavaBasic/Estrutura.java
@@ -0,0 +1,16 @@
+package JavaBasic;
+
+public class Estrutura {
+
+	public static void main(String[] args) {
+		System.out.println("Olá turma, sejam bem-vindos");
+		
+		
+		final int ano = 2001;
+		
+		
+		
+		System.out.println(ano);
+	}
+
+}
